4167d497ca2a4d3361bc50db3b52cf3236e3ff99
AvrilC9 <avrilc8@nycstudents.net>
2024-09-16 16:31:14 -0400

    Create Cao_Avril_L2.cpp

---
+++ Cao_Avril_L2.cpp
@@ -0,0 +1,33 @@
+// Avril Cao
+// Lecture 2
+// September 16, 2024
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+    int numSix = 6;
+    float floNum = 7.11;
+    double douNum = 9.67;
+    char lettA = 'A';
+    bool booleanTru = true;
+    string strHi = "Hi";
+
+    cout << "int: " << numSix <<
+    cout << "float: " << floNum <<
+    cout << "double: " << douNum <<
+    cout << "char: " << lettA <<
+    cout << "bool: " << booleanTru <<
+    cout << "string: " << striHi << 
+
+    int inches; 
+    inches = 100;
+
+    cout << inches << " inches(es) = "; 
+    cout << inches / 12 << " feet (foot) and ";
+    cout << inches % 12 << " inch(es)" << endl;
+
+    return 0;
+}
